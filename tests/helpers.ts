d1f6ab20dfd2e74aba091fc015d708eed8b0ad61
Bartosz Prusinowski <bartoszprusinowski1@gmail.com>
2023-07-09 16:04:15 +0200

    fix: Tests

--- tests/helpers.ts
+++ tests/helpers.ts
@@ -1,9 +1,13 @@
 import { makeSvg } from "../src/components";
 import { Dimensions } from "../src/dims";
+import { DEFAULT_FONT_FAMILY } from "../src/utils";
 
 export const setup = () => {
   const div = document.createElement("div");
-  const svg = makeSvg(div);
+  const svg = makeSvg(div, {
+    svgBackgroundColor: "#FFFFFF",
+    fontFamily: DEFAULT_FONT_FAMILY,
+  });
   const dims = new Dimensions(800, 550);
 
   return { svg, dims };
